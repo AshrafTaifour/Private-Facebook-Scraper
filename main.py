2cdb2fce5e79084d223e04de77825c50ad4fe812
Ashraf Taifour <ashraftayfour@gmail.com>
2021-03-21 16:11:27 -0400

    refractored login, userURL, and likesScraping

--- main.py
+++ main.py
@@ -9,57 +9,21 @@ from secretDirectory import secret
 from tbselenium.tbdriver import TorBrowserDriver
 import os
 
-#username is name in url
+# username is name in url
 
 
 def getFriendsLikes(uname, email, passw, numFriendsToScrape, path):
-    # will turn off notification for FB to allow webscraping for windows
-    chrome_options = webdriver.ChromeOptions()
-    prefs = {"profile.default_content_setting_values.notifications": 2}
-    chrome_options.add_experimental_option("prefs", prefs)
 
     # store original path of the directory (tor is changing the path when executing)
     originalPath = os.getcwd()
 
-    if('chrome' in path):
-        driver = webdriver.Chrome(path, options=chrome_options)
-        driver.get("http://www.facebook.com")
-    elif('tor' in path):
-        # for linux, uses tor browser.
-        driver = TorBrowserDriver(path)
-        driver.get("https://www.facebookcorewwwi.onion/")
-    else:
-        print(f"'tor' or 'chrome' are not in your path's name, please change the name of your chrome driver to include the word chrome in it or the tor directory to include the word tor.")
-        return
-
-    # will open the webpage
-
-    # target username field
-    usernameBox = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='email']")))
-    passwordBox = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='pass']")))
-
-    # enters username and password
-    usernameBox.clear()
-    usernameBox.send_keys(email)
-    passwordBox.clear()
-    passwordBox.send_keys(passw)
-
-    # login button is targeted and clicked
-    button = WebDriverWait(driver, 2).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
-    #
+    # login, visit the facebook page and return the driver
+    driver = loginToFacebook(path, email, passw)
     time.sleep(8)
-    #facebookURL = driver.current_url
 
-    # if account has no real username
-    if 'profile.php?' in uname:
-        print('************************************************************')
-        driver.get(f'https://www.facebookcorewwwi.onion/{uname}&sk=friends')
-        print('**************************************************************')
-    else:
-        driver.get(f"https://www.facebook.com/{uname}/friends_all")
+    yourFriendlistPage = getFriendsListPage(uname)
+
+    driver.get(yourFriendlistPage)
 
     # scroll 10 times to get all friends on page
     for j in range(0, 10):
@@ -74,33 +38,59 @@ def getFriendsLikes(uname, email, passw, numFriendsToScrape, path):
     os.chdir(originalPath)
 
     # will parse the HTML page to obtain hrefs of friends.
-    hrefs = parseHTML(FULLHTMLPAGE, "friendsurls", 1)
-    # will parse hrefs, take only friends' pages and append all_likes to them so we can access their likes.
-    friendURLS = parseURLS(hrefs)
+    friendURLS = parseHTML(FULLHTMLPAGE, "friendsurls", 1)
 
-    if(numFriendsToScrape == 1):
-        driver.get(friendURLS[0])
-        time.sleep(3)
-        # scroll to get the entire 'likes' page
-        for k in range(10):
-            driver.execute_script(
-                "window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(2)
-        likesPage = driver.page_source
-        parseHTML(likesPage, "friendslikes", 1)
+    scrapeLikePages(driver, friendURLS, numFriendsToScrape)
+
+
+def loginToFacebook(exePath, useremail, userpass):
+    if('chrome' in exePath):
+        # will turn off notification for FB to allow webscraping for windows
+        chrome_options = webdriver.ChromeOptions()
+        prefs = {"profile.default_content_setting_values.notifications": 2}
+        chrome_options.add_experimental_option("prefs", prefs)
+        driver = webdriver.Chrome(exePath, options=chrome_options)
+        # will open the webpage
+        driver.get("http://www.facebook.com")
+    elif('tor' in exePath):
+        # for linux, uses tor browser.
+        driver = TorBrowserDriver(exePath)
+        # will open the webpage
+        driver.get("https://www.facebookcorewwwi.onion/")
     else:
-        i = 0
-        while(i < numFriendsToScrape and i < len(friendURLS)):
-            driver.get(friendURLS[i])
-            # scroll to get the entire 'likes' page
-            for k in range(10):
-                driver.execute_script(
-                    "window.scrollTo(0, document.body.scrollHeight);")
-                time.sleep(2)
-            time.sleep(3)
-            likesPage = driver.page_source
-            parseHTML(likesPage, "friendslikes", i)
-            i += 1
+        print(f"'tor' or 'chrome' are not in your path's name, please change the name of your chrome driver to include the word chrome in it or the tor directory to include the word tor.")
+        return
+
+    # targets username and password boxes
+    usernameBox = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='email']")))
+    passwordBox = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='pass']")))
+
+    # enters username and password
+    usernameBox.clear()
+    usernameBox.send_keys(useremail)
+    passwordBox.clear()
+    passwordBox.send_keys(userpass)
+
+    # login button is targeted and clicked
+    button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, "button[type='submit']"))).click()
+
+    return driver
+
+# will return the URL of the user's friends list page based on whether they have a user name or not
+
+
+def getFriendsListPage(uname):
+    # if account has no real username
+    retStr = ""
+    if 'profile.php?' in uname:
+        retStr = f'https://www.facebookcorewwwi.onion/{uname}&sk=friends'
+    else:
+        retStr = f"https://www.facebook.com/{uname}/friends_all"
+
+    return retStr
 
 
 # will parse pagesource and return a list of HREFs, parses through friends list page and a friend's likes page
@@ -113,6 +103,7 @@ def parseHTML(HTMLPAGE, typeOfPage: str, friendNumber: int):
     if(typeOfPage == "friendsurls"):
         # oajrlxb2 class contains many URLS that will be useful for finding friends' page URLs
         containers = page_soup.findAll("a", {"class": "oajrlxb2"})
+
         # save the page to local storage for local parsing
         with open("friendListPage.html", "w", encoding='utf-8') as file:
             file.write(str(containers))
@@ -120,12 +111,14 @@ def parseHTML(HTMLPAGE, typeOfPage: str, friendNumber: int):
         tree = html.parse("friendListPage.html")
         html.tostring(tree)
         hrefs = tree.xpath('//@href')
+        # will parse hrefs, take only friends' pages and append all_likes to them so we can access their likes.
+        hrefs = parseURLS(hrefs)
 
     # obtain a friend's likes
     elif(typeOfPage == "friendslikes"):
         # d2edcug0 span contains the name of the liked page
         containers = page_soup.findAll("span", {"class": "d2edcug0"})
-        print(f"\n \n \n \n {containers} \n \n \n \n ")
+
         # save the page to local storage for local parsing
         with open(f"friendLikesPage{friendNumber}.html", "w", encoding='utf-8') as file:
             file.write(str(containers))
@@ -151,17 +144,33 @@ def parseURLS(lst: list) -> list:
     return newLst
 
 
+def scrapeLikePages(driver, friendURLs, numOfFriends):
+    i = 0
+    while(i < numOfFriends and i < len(friendURLs)):
+        driver.get(friendURLs[i])
+        time.sleep(3)
+        # scroll to get the entire 'likes' page
+        for k in range(10):
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(2)
+
+        likesPage = driver.page_source
+        parseHTML(likesPage, "friendslikes", i)
+        i += 1
+
+
 if __name__ == '__main__':
 
     email = secret.EMAIL
     uname = secret.UNAME
     passw = secret.passw
-    numOfFriendsToScrape = 2
-    #windowsPath = secret.windowsPath
-    linuxPath = secret.linuxPath
-    # gets friendsList page
-    #getFriendsLikes(uname, email, passw, numOfFriendsToScrape, windowsPath)
-    getFriendsLikes(uname, email, passw, numOfFriendsToScrape, linuxPath)
+    numOfFriendsToScrape = 3
 
-    # print(email)
-    # print(passw)
+    # Windows Scraping
+    # windowsPath = secret.windowsPath
+    # getFriendsLikes(uname, email, passw, numOfFriendsToScrape, windowsPath)
+
+    # Linux Scraping
+    linuxPath = secret.linuxPath
+    getFriendsLikes(uname, email, passw, numOfFriendsToScrape, linuxPath)
